eb3f29fd000ad5e96f8b70793b4814cbeef069a9
Greg P <gorgarp@gmail.com>
2019-10-09 19:07:22 -0500

    temp change to local repo to test

--- web-install.sh
+++ web-install.sh
@@ -36,5 +36,5 @@ cd $TARGET
 echo "Now we download and execute the build script"
 echo ""
 
-bash <(curl -s https://raw.githubusercontent.com/markus-perl/ffmpeg-build-script/master/build-ffmpeg) --build
+bash <(curl -s https://raw.githubusercontent.com/gorgarp/ffmpeg-build-script/master/build-ffmpeg) --build
 
