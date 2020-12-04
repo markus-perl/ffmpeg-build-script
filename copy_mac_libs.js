2c978b2d4dac471c3bcfa6314e1e09b7cf1f5cb3
Steve Rubin <nerdforlife@gmail.com>
2020-12-04 13:17:34 -0800

    more cleanup

--- copy_mac_libs.js
+++ copy_mac_libs.js
@@ -1,5 +1,5 @@
 const { resolve, basename } = require('path');
-const { mkdirSync } = require('fs');
+const { mkdirSync, readlinkSync, readdirSync } = require('fs');
 const { execSync } = require('child_process');
 
 function logAndExec(cmd) {
@@ -7,23 +7,29 @@ function logAndExec(cmd) {
   execSync(cmd);
 }
 
+const baseIncludesDir = resolve(__dirname, 'workspace/include');
 const baseBinDir = resolve(__dirname, 'workspace/bin');
 const baseLibDir = resolve(__dirname, 'workspace/lib');
 const destDir = resolve(__dirname, 'workspace/mac');
 
-logAndExec(`rm -r ${destDir}`);
+try {
+  logAndExec(`rm -r ${destDir}`);
+} catch (err) {
+  //
+}
 logAndExec(`mkdir -p ${destDir}`);
 
 const skippedLibs = new Set();
 const copiedLibs = new Set();
 const missingLibs = new Set();
+const namesWithoutVersion = new Set();
 
 function copyDylibs(binaryName, base = baseBinDir) {
   const origPath = resolve(base, binaryName);
   const binaryPath = resolve(destDir, binaryName);
-  logAndExec(`cp ${origPath} ${binaryPath}`);
 
-  console.log(`Inspecting ${binaryPath}`);
+  logAndExec(`cp -a ${origPath} ${binaryPath}`);
+
   const lines = execSync(`otool -L ${binaryPath}`).toString('utf8').split('\n');
   const libsToRewrite = [];
   for (const line of lines) {
@@ -41,8 +47,17 @@ function copyDylibs(binaryName, base = baseBinDir) {
         copiedLibs.add(path);
         copiedLibs.add(newFilename);
         if (path !== newFilename) {
-          logAndExec(`cp ${path} ${newFilename}`);
-          logAndExec(`chmod u+w ${newFilename}`);
+          // logAndExec(`cp ${path} ${newFilename}`);
+
+          // copy sym-linked libraries as well
+          let nameWithoutVersion = filename.split('.')[0];
+          if (filename.includes('libSDL2')) {
+            nameWithoutVersion = 'libSDL2';
+          }
+          namesWithoutVersion.add(nameWithoutVersion);
+          const nameWithoutVersionLib = `${nameWithoutVersion}.dylib`;
+          logAndExec(`cp -a ${resolve(baseLibDir, nameWithoutVersion)}*.dylib ${destDir}/.`);
+
           copyDylibs(filename, baseLibDir);
         }
       }
@@ -51,13 +66,26 @@ function copyDylibs(binaryName, base = baseBinDir) {
       skippedLibs.add(path);
     }
   }
+
+  // find the non-sym-linked version of this library
+  let actualBinaryPath = binaryPath;
+  try {
+    const actualBinaryName = readlinkSync(binaryPath);
+    actualBinaryPath = resolve(destDir, actualBinaryName);
+  } catch (err) {
+    //
+  }
+
   if (libsToRewrite.length > 0) {
-    logAndExec(`install_name_tool -id @loader_path/${binaryName} ${libsToRewrite.map(({path, filename}) => `-change ${path} @loader_path/${filename}`).join(' ')} ${binaryPath}`);
+    logAndExec(`install_name_tool -id @loader_path/${binaryName} ${libsToRewrite.map(({path, filename}) => `-change ${path} @loader_path/${filename}`).join(' ')} ${actualBinaryPath}`);
   }
 }
 
 copyDylibs('ffmpeg');
 copyDylibs('ffprobe');
+
+console.log('Copying includes');
+logAndExec(`cp -r ${baseIncludesDir} ${destDir}/.`);
 
 for (const lib of Array.from(skippedLibs).sort()) {
   console.log(`[NOTE] skipped ${lib}`);
