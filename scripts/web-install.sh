860914bcf4fa117151467492b1184215becccdf8
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-12-27 17:41:39 -0500

    Update web-install.sh

--- scripts/web-install.sh
+++ scripts/web-install.sh
@@ -4,9 +4,9 @@
 
 make_dir()
 {
-    if [ ! -d "$1" ]; then
-        if ! mkdir "$1"; then
-            printf "\n Failed to create dir %s" "$1";
+    if [ ! -d "${1}" ]; then
+        if ! mkdir -p "${1}"; then
+            printf "\n Failed to create dir %s" "${1}";
             exit 1
         fi
     fi
@@ -14,23 +14,25 @@ make_dir()
 
 command_exists()
 {
-    if ! [[ -x $(command -v "$1") ]]; then return 1; fi
+    if ! [[ -x "$(command -v "${1}")" ]]; then return 1; fi
     return 0
 }
 
 TARGET='ffmpeg-build'
 
-if ! command_exists "curl"; then
-    echo "curl not installed.";
+if ! command_exists 'curl'; then
+    echo 'curl not installed.';
     exit 1
 fi
 
-echo "ffmpeg-build-script-downloader v0.1"
-echo -e "=========================================\\n"
+echo 'ffmpeg-build-script-downloader v0.1'
+echo '========================================='
+echo
 
-echo "First we create the ffmpeg build directory $TARGET"
-make_dir $TARGET
-cd $TARGET || return
+echo "First we create the ffmpeg build directory ${TARGET}"
+make_dir "${TARGET}"
+cd "${TARGET}" || return 1
 
-echo -e "Now we download and execute the build script\\n"
-bash <(curl -s https://raw.githubusercontent.com/slyfox1186/ffmpeg-build-script/master/scripts/build-ffmpeg) --build
+echo 'Now we download and execute the build script'
+echo
+bash <(curl -s 'https://raw.githubusercontent.com/slyfox1186/ffmpeg-build-script/master/scripts/build-ffmpeg') --build
